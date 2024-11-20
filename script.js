340522e8b14391d7e0abefe06f45d4c3087f2bba
Yaseen <169384643+n7a1b4kx@users.noreply.github.com>
2024-11-20 14:51:35 +0500

    fix: delete code repeatation and place func call in loop
    deleted console log feature in a func to avoid repetition
    also change the const type to var so It can be reassigned during the runtime
    as this will also call func at every round asking for human input

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function greeting() {
     // \n or backslash n create or omits a newline
     // \t or tab omits or add a tab 4 spaces 
     console.log("Hello, let's play a little game. Shall we?\nRock Paper Scissor a classic game!\nRules are simple, you have three choices rock, paper, and scissor choose which one you desire.\nBut choose wisely:\n\tRock beats scissor but paper beats it.\n\tPaper beats Rock but scissor beats it.\n\tScissor beats paper but rock beats it.");
+    console.log("\t======Game Start======");
 }
 
 
@@ -33,28 +34,36 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-function playRound(humanScore, computerScore) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
+function choiceResult(humanChoice, computerChoice) {
+    console.log(`Human choosed ${humanChoice} / Computer Choosed ${computerChoice}.`);
+}
 
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
     // checking for round winner and incrementing it's score!!!
     if ( (humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissor") || (humanChoice === "scissor" && computerChoice === "rock") ) {
-        console.log(`Human choosed ${humanChoice} / Computer Choosed ${computerChoice}.`);
+        choiceResult(humanChoice, computerChoice);
         console.log("\tComputer Wins!");
         computerScore += 1;
     } else if ( (humanChoice === "rock" && computerChoice === "scissor") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissor" && computerChoice === "paper")) {
-        console.log(`Human choosed ${humanChoice} / Computer Choosed ${computerChoice}.`);
+        choiceResult(humanChoice, computerChoice);
         console.log("\tHuman Wins!");
         humanScore += 1;
-    }
+    } else if (humanChoice === computerChoice) {
+        choiceResult(humanChoice, computerChoice);
+        console.log("\tDraw");
+    }   
 }
 
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
     greeting();
     for (let i = 0; i < 5; i++) {
-        playRound(humanScore, computerScore);
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
     }
     console.log(`Human Scored: ${humanScore}\nComputer Scored: ${computerScore}`);
 }
